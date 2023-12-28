43154bb70ba0e1a80574122dafa6be97ec04d4d7
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2023-12-28 03:47:05 -0500

    moved code from character functions to types function. added validation to character types. moved global variales into local types function

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,6 @@ const generateBtn = document.querySelector("#generate");
 
 //global variables.
 let number;
-let loArr = [];
-let upArr = [];
-let numArr = [];
-let specialArr = [];
 
 
 // Write password to the #password input
@@ -21,8 +17,7 @@ function writePassword() {
 function generatePassword() {
   
   length();
-  types();
-  return createPassword();
+  return types();
 
 }
 
@@ -46,95 +41,75 @@ function types() {
   let lowercase;
   let uppercase;
   let numeric;
-  let specialCharacters;  
+  let specialCharacters;
+  let passwordArr = [];
+  let passwordArr2 = [];
+  let password1 = '';
 
   lowercase = confirm ('Include lowercase?');
   if (lowercase) {
-    getLocase();
+    
+    const abc = ['a', 'b','c','d','e','f','g','h','i','j','k',
+    'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  
+    for (i = 0; i < number; i++) {
+      index = Math.floor(Math.random() * abc.length);
+      passwordArr.push(abc[index]);
+    }   
   }
+
+
   uppercase = confirm ('Include uppercase?');
   if (uppercase) {
-    getUpcase();
+    
+    const ABC = ['A', 'B','C','D','E','F','G','H','I','J','K',
+    'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  
+    for (i = 0; i < number; i++) {
+      index = Math.floor(Math.random() * ABC.length);
+      passwordArr.push(ABC[index]);
+    }
   }
+
+
   numeric = confirm ('Include numbers?');
   if (numeric) {
-    getNum();
+    
+    const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    for (i = 0; i < number; i++) {
+      index = Math.floor(Math.random() * num.length);
+      passwordArr.push(num[index]);
+    }
   }
+
+  
   specialCharacters = confirm ('Include special characters?');
   if (specialCharacters) {
-    getSpecial();
+   
+    const special = ['!',  '"',  '#',  '$',  '%',  '&',  "'",  '(',  ')',  '*',  '+',  ',',  '-',  '.',  '/',  ':',  ';',  '<',  '=',  '>',  '?',  '@',  '[',    ']',  '^',  '_',  '`',  '{',  '|',  '}',  '~', "\\" ];
+
+    for (i = 0; i < number; i++) {
+      index = Math.floor(Math.random() * special.length);
+      passwordArr.push(special[index]);
+    }
   }
 
-}
 
-//selects lowercase characters from an array.
-function getLocase() {
-  
-  const abc = ['a', 'b','c','d','e','f','g','h','i','j','k',
-  'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-  for (i = 0; i < number; i++) {
-    index = Math.floor(Math.random() * abc.length);
-    loArr.push(abc[index]);
-  }         
-
-}
-
-//selects uppercase characters from an array.
-function getUpcase() {
-  
-  const ABC = ['A', 'B','C','D','E','F','G','H','I','J','K',
-  'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
-  for (i = 0; i < number; i++) {
-    index = Math.floor(Math.random() * ABC.length);
-    upArr.push(ABC[index]);
+  if (!lowercase && !uppercase && !numeric && !specialCharacters) {
+    alert ('Choose at least one character type!')
+    return types();
   }
 
-}
-
-//selects numbers from an array.
-function getNum() {
-  
-  const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
   for (i = 0; i < number; i++) {
-    index = Math.floor(Math.random() * num.length);
-    numArr.push(num[index]);
+    index = Math.floor(Math.random() * passwordArr.length);
+    passwordArr2.push(passwordArr[index]);
   }
 
-}
-
-//selects special characters from an array.
-function getSpecial() {
-  
-  const special = ['!',  '"',  '#',  '$',  '%',  '&',  "'",  '(',  ')',  '*',  '+',  ',',  '-',  '.',  '/',  ':',  ';',  '<',  '=',  '>',  '?',  '@',  '[',    ']',  '^',  '_',  '`',  '{',  '|',  '}',  '~', "\\" ];
-
-  for (i = 0; i < number; i++) {
-    index = Math.floor(Math.random() * special.length);
-    specialArr.push(special[index]);
-  }
-  // console.log(specialArr.join("")); 
-}
-
-
-//combines all character type arrays into a single array,
-//and selects random elements from the combines array.
-function createPassword() {
-  
-  let passwordArr = [];
-  let password1 = '';
-  
-  combChar = loArr.concat(upArr).concat(numArr).concat(specialArr);
-
-  for (i = 0; i < number; i++) {
-    index = Math.floor(Math.random() * combChar.length);
-    passwordArr.push(combChar[index]);
-  }
-  password1 = passwordArr.join("")
+  password1 = passwordArr2.join("")
   console.log(password1);
   return password1;
-  
+
 }
 
 // Add event listener to generate button
